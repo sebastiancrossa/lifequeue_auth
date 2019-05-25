cc4fb42c70289680fbbc08931d0789f2cf563ddb
Sebastian Crossa <crossasebastian@gmail.com>
2019-05-25 00:05:09 -0600

    Added sign up view

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Layout from './hoc/layout/Layout';
 import Home from './containers/Home/Home';
 import Todos from './containers/Todos/Todos';
 import Login from './containers/Auth/Login/Login';
+import Signup from './containers/Auth/Signup/Signup';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route exact path='/' component={withRouter(Home)} />
         <Route exact path='/todos' component={withRouter(Todos)} />
         <Route exact path='/login' component={withRouter(Login)} />
+        <Route exact path='/signup' component={withRouter(Signup)} />
 
         {/* Every route that doesn't match any of the above will redirect to home */}
         <Redirect to='/' />
