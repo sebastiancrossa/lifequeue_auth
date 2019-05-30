9ca3495f3b6007fbf1b54cec76434b4db068dfd3
Sebastian Crossa <crossasebastian@gmail.com>
2019-05-29 23:05:31 -0600

    Users can resend the verification email

--- src/containers/Auth/VerifyEmail/VerifyEmail.js
+++ src/containers/Auth/VerifyEmail/VerifyEmail.js
@@ -1,22 +1,73 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../../store/actions';
+import styled from 'styled-components';
 
 //* Component imports
 import { FormWrapper } from '../../../hoc/layout/elements';
 import Heading from '../../../components/UI/Headings/Heading';
+import Button from '../../../components/UI/Forms/Button/Button';
+import Message from '../../../components/UI/Message/Message';
 
-const VerifyEmail = () => {
+const MessageWrapper = styled.div`
+  position: absolute;
+  bottom: -3rem;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const VerifyEmail = ({ sendVerification, loading, error }) => {
   return (
     <FormWrapper>
-      <Heading color='white' size='h1'>
-        Please verify your email.
-      </Heading>
+      <Wrapper>
+        <Heading color='white' size='h1'>
+          Please verify your email.
+        </Heading>
 
-      <Heading color='white' bold size='h4'>
-        Check your email box and verify your email by clicking the link
-        provided.
-      </Heading>
+        <Heading color='white' bold size='h4'>
+          Check your email box and verify your email.
+        </Heading>
+
+        <Button
+          loading={loading ? 'Sending email verification...' : null}
+          onClick={() => sendVerification()}
+          disabled={loading}
+        >
+          Re-send verification email
+        </Button>
+
+        <MessageWrapper>
+          <Message error show={error}>
+            {error}
+          </Message>
+          <Message success show={error === false}>
+            Email verification sent succesfully
+          </Message>
+        </MessageWrapper>
+      </Wrapper>
     </FormWrapper>
   );
 };
 
-export default VerifyEmail;
+const mapStateToProps = ({ auth }) => ({
+  loading: auth.verifyEmail.loading,
+  error: auth.verifyEmail.error
+});
+
+const mapDispatchToProps = {
+  sendVerification: actions.verifyEmail
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(VerifyEmail);
--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -63,3 +63,22 @@ export const signOut = () => async (dispatch, getState, { getFirebase }) => {
 export const clean = () => ({
   type: actions.CLEAN_UP
 });
+
+// Verify email actions
+export const verifyEmail = () => async (
+  dispatch,
+  getState,
+  { getFirebase }
+) => {
+  const firebase = getFirebase();
+
+  dispatch({ type: actions.VERIFY_START });
+  try {
+    const user = firebase.auth().currentUser;
+    await user.sendEmailVerification();
+
+    dispatch({ type: actions.VERIFY_SUCCESS });
+  } catch (err) {
+    dispatch({ type: actions.VERIFY_FAIL, payload: err.message });
+  }
+};
